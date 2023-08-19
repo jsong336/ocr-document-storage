145e05e334817322656fe7cfd64c8f786bbdac7a
Jeongwon Song <jeongwon412@gmail.com>
2023-08-19 14:57:05 -0400

    added boilerplate
    Signed-off-by: Jeongwon Song <jeongwon412@gmail.com>

---
+++ server/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setup(
+    name="chak.io",
+    version="0.0.0",
+    description="chak.io server",
+    long_description=long_description,
+    author="Jeongwon Song",
+    author_email="jeongwon412@gmail.com",
+    packages=["chak"],  # same as name
+    python_requires=">=3.10.0",
+    install_requires=[
+        "django", 
+        "psycopg2-binary"
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "black",
+        ]
+    },
+)
